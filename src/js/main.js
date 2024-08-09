9df70699f2e2f07385e8f6d30d091b26be9e2353
Luciano Ayres <lucianofarias@gmail.com>
2024-08-08 21:51:29 -0300

    style: add error state

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 console.log("hello world")
 
+const mainTop = document.querySelector(".main__top")
 const tabs = document.querySelector(".main__tabs")
 
 const loading = document.querySelector(".main__loading")
+
+const error = document.querySelector(".main__error")
 
 const code = document.querySelector(".main__code")
 const company = document.querySelector(".main__company")
@@ -11,8 +14,12 @@ const disclaimer = document.querySelector(".main__disclaimer")
 // loading state hidden
 loading.style.display = "none"
 
+// error state hidden
+// error.style.display = "none"
+mainTop.style.visibility = "hidden"
+
 // loaded state hidden
-// tabs.style.visibility = "hidden"
-// code.style.visibility = "hidden"
-// company.style.visibility = "hidden"
-// disclaimer.style.visibility = "hidden"
+tabs.style.visibility = "hidden"
+code.style.visibility = "hidden"
+company.style.visibility = "hidden"
+disclaimer.style.visibility = "hidden"
