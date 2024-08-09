aad9a0c57d7084bb5c4b4ef1592aef6b0701b774
Luciano Ayres <lucianofarias@gmail.com>
2024-08-08 21:36:23 -0300

    style: add loading state animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 console.log("hello world")
+
+const tabs = document.querySelector(".main__tabs")
+
+const loading = document.querySelector(".main__loading")
+
+const code = document.querySelector(".main__code")
+const company = document.querySelector(".main__company")
+const disclaimer = document.querySelector(".main__disclaimer")
+
+// loading state hidden
+// loading.style.display = "none"
+
+// loaded state hidden
+tabs.style.visibility = "hidden"
+code.style.visibility = "hidden"
+company.style.visibility = "hidden"
+disclaimer.style.visibility = "hidden"
