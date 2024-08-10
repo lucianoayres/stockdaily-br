b4433e0b0b61bbb596b662f2fdb71b4c5610d8d1
Luciano Ayres <lucianofarias@gmail.com>
2024-08-10 20:34:35 -0300

    style: update yesterday tab style

--- src/js/main.js
+++ src/js/main.js
@@ -16,6 +16,13 @@ const STOCK_DATA_URL = "https://raw.githubusercontent.com/lucianoayres/random-st
 const ACTIVE_TAB_CLASSNAME = "main__tab--active"
 const DISABLED_TAB_CLASSNAME = "main__tab--disabled"
 
+const TODAY_TAB_TITLE = "View Today's Stock Ticker"
+const YESTERDAY_TAB_TITLE = "View Yesterday's Stock Ticker"
+const YESTERDAY_TAB_NO_DATA_TITLE = "No stock data for yesterday"
+
+elements.todayTab.setAttribute("title", TODAY_TAB_TITLE)
+elements.yesterdayTab.setAttribute("title", YESTERDAY_TAB_TITLE)
+
 // Utility functions
 const setElementVisibility = (element, visibility) => {
     if (element) element.style.visibility = visibility
@@ -32,11 +39,11 @@ const updateElementText = (element, text) => {
 const toggleTabState = (tab, disable) => {
     if (disable) {
         tab.classList.add(DISABLED_TAB_CLASSNAME)
-        tab.setAttribute("title", "No stock data for yesterday")
+        tab.setAttribute("title", YESTERDAY_TAB_TITLE)
         tab.setAttribute("aria-disabled", "true")
     } else {
         tab.classList.remove(DISABLED_TAB_CLASSNAME)
-        tab.removeAttribute("title")
+        tab.setAttribute("title", YESTERDAY_TAB_TITLE)
         tab.removeAttribute("aria-disabled")
     }
 }
