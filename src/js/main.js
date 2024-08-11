a2a7b9fe4d60e908f303fad162ec6c2481871008
Luciano Ayres <lucianofarias@gmail.com>
2024-08-10 21:03:09 -0300

    feat: add new view random ticker feature

--- src/js/main.js
+++ src/js/main.js
@@ -9,19 +9,24 @@ const elements = {
     disclaimer: document.querySelector(".main__disclaimer"),
     todayTab: document.getElementById("today-tab"),
     yesterdayTab: document.getElementById("yesterday-tab"),
+    randomTab: document.getElementById("random-tab"),
+    tryAgainButton: document.getElementById("reload-button"),
 }
 
 // Constants
 const STOCK_DATA_URL = "https://raw.githubusercontent.com/lucianoayres/random-stock-of-the-day-json/main/b3-last-picks.json"
+const RANDOM_DATA_URL = "https://raw.githubusercontent.com/lucianoayres/brazil-stocks-tickers-json/main/b3_stocks_tickers.json" // Separate URL for random data
 const ACTIVE_TAB_CLASSNAME = "main__tab--active"
 const DISABLED_TAB_CLASSNAME = "main__tab--disabled"
 
 const TODAY_TAB_TITLE = "View Today's Stock Ticker"
 const YESTERDAY_TAB_TITLE = "View Yesterday's Stock Ticker"
+const RANDOM_TAB_TITLE = "View a Random Stock Ticker"
 const YESTERDAY_TAB_NO_DATA_TITLE = "No stock data for yesterday"
 
 elements.todayTab.setAttribute("title", TODAY_TAB_TITLE)
 elements.yesterdayTab.setAttribute("title", YESTERDAY_TAB_TITLE)
+elements.randomTab.setAttribute("title", RANDOM_TAB_TITLE)
 
 // Utility functions
 const setElementVisibility = (element, visibility) => {
@@ -75,10 +80,16 @@ const showErrorState = () => {
     setElementVisibility(elements.mainTop, "hidden")
 }
 
+// Random item picker
+const getRandomItem = (data) => {
+    const randomIndex = Math.floor(Math.random() * data.length)
+    return data[randomIndex]
+}
+
 // Data fetching function
-const fetchData = async (index = -1) => {
+const fetchData = async ({ url, mode } = {}) => {
     try {
-        const response = await fetch(STOCK_DATA_URL)
+        const response = await fetch(url)
         if (!response.ok) throw new Error("Network response was not ok")
 
         const data = await response.json()
@@ -86,13 +97,19 @@ const fetchData = async (index = -1) => {
 
         toggleTabState(elements.yesterdayTab, data.length === 1)
 
-        const selectedItem = index === -1 ? data[data.length - 1] : data[data.length - 2]
-        const { ticker: code, name: company } = selectedItem
+        const modeActions = {
+            today: () => data[data.length - 1],
+            yesterday: () => (data.length > 1 ? data[data.length - 2] : null),
+            random: () => getRandomItem(data),
+        }
 
-        if (!code || !company) throw new Error("Invalid data: code or company is missing")
+        const selectedItem = modeActions[mode]()
+        if (!selectedItem || !selectedItem.ticker || !selectedItem.name) {
+            throw new Error("Invalid data: code or company is missing")
+        }
 
-        updateElementText(elements.code, code)
-        updateElementText(elements.company, company)
+        updateElementText(elements.code, selectedItem.ticker)
+        updateElementText(elements.company, selectedItem.name)
         showDataState()
     } catch (error) {
         console.error("Error fetching data:", error)
@@ -101,25 +118,28 @@ const fetchData = async (index = -1) => {
 }
 
 // Tab switching function
-const switchTab = (activeTab, inactiveTab, index) => {
+const switchTab = (activeTab, inactiveTabs, mode, url) => {
     activeTab.classList.add(ACTIVE_TAB_CLASSNAME)
-    inactiveTab.classList.remove(ACTIVE_TAB_CLASSNAME)
+    inactiveTabs.forEach((tab) => tab.classList.remove(ACTIVE_TAB_CLASSNAME))
     showLoadingState()
-    fetchData(index)
+    fetchData({ mode, url })
 }
 
 // Event listeners
-elements.todayTab.addEventListener("click", () => switchTab(elements.todayTab, elements.yesterdayTab, -1))
+elements.todayTab.addEventListener("click", () => switchTab(elements.todayTab, [elements.yesterdayTab, elements.randomTab], "today", STOCK_DATA_URL))
 elements.yesterdayTab.addEventListener("click", () => {
     if (!elements.yesterdayTab.classList.contains(DISABLED_TAB_CLASSNAME)) {
-        switchTab(elements.yesterdayTab, elements.todayTab, -2)
+        switchTab(elements.yesterdayTab, [elements.todayTab, elements.randomTab], "yesterday", STOCK_DATA_URL)
     }
 })
+elements.randomTab.addEventListener("click", () => switchTab(elements.randomTab, [elements.todayTab, elements.yesterdayTab], "random", RANDOM_DATA_URL)) // Updated event listener for randomTab
+
+elements.tryAgainButton.addEventListener("click", () => location.reload())
 
 // Initialization
 const init = () => {
     showLoadingState()
-    fetchData() // Load the last item (today's data) by default
+    fetchData({ mode: "today", url: STOCK_DATA_URL }) // Load the last item (today's data) by default
 }
 
 init()
